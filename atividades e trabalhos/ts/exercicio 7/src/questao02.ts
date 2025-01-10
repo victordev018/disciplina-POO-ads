a3d9d7b97838e6febf426cd3a82cac9b39edea71
João VIctor <joawrt8520@gmail.com>
2025-01-10 05:15:23 -0300

    feat: resolvida questão 02 do exercicio 07 - classe Calculadora

---
+++ atividades e trabalhos/ts/exercicio 7/src/questao02.ts
@@ -0,0 +1,26 @@
+class Calculator {
+
+    private numberOne: number;
+    private numberTwo: number;
+
+    constructor(numberOne: number, numberTwo: number) {
+        this.numberOne = numberOne;
+        this.numberTwo = numberTwo;
+    }
+
+    // method to sum values
+    public sum() : number {
+        return this.numberOne + this.numberTwo;
+    }
+}
+
+
+function main() {
+
+    const calc = new Calculator(5, 10);
+    const sum = calc.sum();
+
+    console.log("sum: ", sum);
+}
+
+main();
